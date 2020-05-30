14063b21cd45f282dada3cfd903fd98048a16b7f
James Aley <james.aley@gmail.com>
2020-05-30 15:47:48 +0100

    Adding some route placeholders and basic ui

--- sonos-proxy/src/sonos_proxy/api.clj
+++ sonos-proxy/src/sonos_proxy/api.clj
@@ -1,30 +1,32 @@
 (ns sonos-proxy.api
     (:require [compojure.core           :refer [routes POST GET]]
-              [compojure.route          :refer [not-found]]
+              [compojure.route          :refer [not-found resources]]
               [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+              [ring.util.response       :as resp]
               [taoensso.timbre          :as log]
               
               [sonos-proxy.auth         :as auth]))
 
-(defn secure-routes
-    "Routes requiring authentication"
-    []
-    (auth/wrap-auth
-        (routes
-            (GET "/main" [] "Logged in!"))))
-
-(defn open-routes
-    "Routes that won't be behind OAuth login"
-    []
-    (routes
-        (GET "/" [] "All is very good")))
+(defn static
+    "Return a static page from resources"
+    [html]
+    (-> html
+        (resp/resource-response {:root "html"})
+        (resp/content-type "text/html")))
 
 (defn build-routes
     "Compose routes and apply global settings"
     []
     (let [settings (-> site-defaults (assoc-in [:session :cookie-attrs :same-site] :lax))]
         (-> (routes
-                (open-routes)
-                (secure-routes)
+                (GET "/" [] (static "main.html"))
+                (resources "/css" {:root "css"})
+                (resources "/js" {:root "js"})
+                (GET "/auth/msa/logout" [] (not-found "[not implemented] msa logout"))
+                (GET "/auth/sonos/logout" [] (not-found "[not implemented] sonos logout"))
+                (auth/wrap-auth
+                    (routes
+                        (GET "/auth/sonos/login" [] (not-found "[not implemented] sonos login")
+                        (GET "/api/state" [] (not-found "[not implemented] player state")))))
                 (not-found "Not Found"))
             (wrap-defaults settings))))
